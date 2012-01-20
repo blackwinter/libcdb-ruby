dc39ebf88cd2e54f9c5ba4174b37d33aa7cb576f
Jens Wille <jens.wille@uni-koeln.de>
2012-01-20 12:43:50 +0100

    spec/libcdb/reader_spec.rb: Added some more examples.

--- spec/libcdb/reader_spec.rb
+++ spec/libcdb/reader_spec.rb
@@ -55,12 +55,36 @@ describe LibCDB::CDB::Reader do
       @db.size.should == 0
     end
 
+    it "should know its total" do
+      @db.total.should == 0
+    end
+
+    it "should know its keys" do
+      @db.keys.should == []
+    end
+
+    it "should know its values" do
+      @db.values.should == []
+    end
+
     it "should know if it doesn't have a key" do
       @db.should_not have_key('key3')
     end
 
     it "should know if it doesn't have a value" do
       @db.should_not have_value('value3.2')
+    end
+
+    it "should dump itself" do
+      @db.dump.should == ''
+    end
+
+    it "should convert itself into a hash" do
+      @db.to_h.should == {}
+    end
+
+    it "should convert itself into an array" do
+      @db.to_a.should == []
     end
 
   end
@@ -85,6 +109,18 @@ describe LibCDB::CDB::Reader do
 
     it "should know its size" do
       @db.size.should == 11
+    end
+
+    it "should know its total" do
+      @db.total.should == 56
+    end
+
+    it "should know its keys" do
+      @db.keys.should == TEST_DATA.map { |k, _| k }.uniq
+    end
+
+    it "should know its values" do
+      @db.values.should == TEST_DATA.map { |_, v| v }.flatten
     end
 
     it "should know if it has a key" do
@@ -135,6 +171,24 @@ describe LibCDB::CDB::Reader do
 
     it "should not find the key for a non-existent value" do
       @db.key('value33.22').should be_nil
+    end
+
+    it "should dump records for key" do
+      d = []
+      @db.each_dump('key3') { |e| d << e }
+      d.should == %w[+4,8:key3->value3.1 +4,8:key3->value3.2 +4,8:key3->value3.3]
+    end
+
+    it "should convert itself into a hash" do
+      h = {}
+      TEST_DATA.each { |k, v| h[k] = v.size > 1 ? v : v.first }
+      @db.to_h.should == h
+    end
+
+    it "should convert itself into an array" do
+      a = []
+      TEST_DATA.each { |k, v| v.each { |w| a << [k, w] } }
+      @db.to_a.should == a
     end
 
   end
