f2e208e112acc05a6e6419f8eae77e9ac4e4fae3
Jens Wille <jens.wille@gmail.com>
2014-12-05 14:07:18 +0100

    spec/libcdb/reader_spec.rb: No longer need to skip Rubinius. (1649d35)

--- spec/libcdb/reader_spec.rb
+++ spec/libcdb/reader_spec.rb
@@ -80,7 +80,7 @@ describe LibCDB::CDB::Reader do
       @db.should_not be_closed
     end
 
-    it "should know that it's not empty", skip: metadata[:skip_rbx] do
+    it "should know that it's not empty" do
       @db.should_not be_empty
     end
 
@@ -88,15 +88,15 @@ describe LibCDB::CDB::Reader do
       @db.size.should == TEST_DATA.size
     end
 
-    it 'should know its total', skip: metadata[:skip_rbx] do
+    it 'should know its total' do
       @db.total.should == TEST_DATA.values.flatten.size
     end
 
-    it 'should know its keys', skip: metadata[:skip_rbx] do
+    it 'should know its keys' do
       @db.keys.should == TEST_DATA.map { |k, _| k }.uniq
     end
 
-    it 'should know its values', skip: metadata[:skip_rbx] do
+    it 'should know its values' do
       @db.values.should == TEST_DATA.map { |_, v| v }.flatten
     end
 
@@ -108,7 +108,7 @@ describe LibCDB::CDB::Reader do
       @db.should have_key('€ürö')
     end
 
-    it "should know if it doesn't have a key", skip: metadata[:skip_rbx] do
+    it "should know if it doesn't have a key" do
       @db.should_not have_key('none')
     end
 
@@ -120,23 +120,23 @@ describe LibCDB::CDB::Reader do
       @db.should have_value('½a×¾b')
     end
 
-    it "should know if it doesn't have a value", skip: metadata[:skip_rbx] do
+    it "should know if it doesn't have a value" do
       @db.should_not have_value('none')
     end
 
-    it 'should get a single value', skip: metadata[:skip_rbx] do
+    it 'should get a single value' do
       @db['k1'].should == 'v1.1'
     end
 
-    it 'should get a single wide-char value', skip: metadata[:skip_rbx] do
+    it 'should get a single wide-char value' do
       @db['€ürö'].should == '½a×¾b'
     end
 
-    it 'should not get non-existent value', skip: metadata[:skip_rbx] do
+    it 'should not get non-existent value' do
       @db['none'].should be_nil
     end
 
-    it 'should get each value', skip: metadata[:skip_rbx] do
+    it 'should get each value' do
       TEST_DATA.each { |k, o|
         r = []
         @db.each(k) { |v| r << v }
@@ -144,13 +144,13 @@ describe LibCDB::CDB::Reader do
       }
     end
 
-    it 'should get all values', skip: metadata[:skip_rbx] do
+    it 'should get all values' do
       TEST_DATA.each { |k, o|
         @db.fetch(k).should == o
       }
     end
 
-    it 'should get last value', skip: metadata[:skip_rbx] do
+    it 'should get last value' do
       @db.fetch_last('k10').should == 'v10.10'
     end
 
@@ -162,7 +162,7 @@ describe LibCDB::CDB::Reader do
       @db.key('½a×¾b').should == '€ürö'
     end
 
-    it 'should not find the key for a non-existent value', skip: metadata[:skip_rbx] do
+    it 'should not find the key for a non-existent value' do
       @db.key('none').should be_nil
     end
 
@@ -182,31 +182,31 @@ describe LibCDB::CDB::Reader do
       d.should == %w[+8,8:€ürö->½a×¾b]
     end
 
-    it 'should convert itself into a hash', skip: metadata[:skip_rbx] do
+    it 'should convert itself into a hash' do
       h = {}
       TEST_DATA.each { |k, v| h[k] = v.size > 1 ? v : v.first }
       @db.to_h.should == h
     end
 
-    it 'should convert itself into an array', skip: metadata[:skip_rbx] do
+    it 'should convert itself into an array' do
       a = []
       TEST_DATA.each { |k, v| v.each { |w| a << [k, w] } }
       @db.to_a.should == a
     end
 
-    it 'should yield keys', skip: metadata[:skip_rbx] do
+    it 'should yield keys' do
       @db.each_key { |k| break k }.should == 'k1'
     end
 
-    it 'should return an enumerator for #each_key without block', skip: metadata[:skip_rbx] do
+    it 'should return an enumerator for #each_key without block' do
       @db.each_key.should be_a(Enumerator)
     end
 
-    it 'should yield values', skip: metadata[:skip_rbx] do
+    it 'should yield values' do
       @db.each_value { |v| break v }.should == 'v1.1'
     end
 
-    it 'should return an enumerator for #each_value without block', skip: metadata[:skip_rbx] do
+    it 'should return an enumerator for #each_value without block' do
       @db.each_value.should be_a(Enumerator)
     end
 
